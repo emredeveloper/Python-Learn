a1ef9fcfea4b584548c917e26deedd2b505a3d03
emredeveloper <karatasqemre@gmail.com>
2023-05-01 00:06:49 +0300

    update 0.1.4

---
+++ Pytq6/app.py
@@ -0,0 +1,78 @@
+def ornek1():
+    """
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        button = QPushButton("Press Me!")
+        button.setCheckable(True)
+        # button.clicked.connect(self.the_button_was_clicked)
+        # button.clicked.connect(self.togled)
+        button.clicked.connect(self.the_button_was_clickedme)
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(button)
+
+    def the_button_was_clicked(self):
+        print("Clicked!")
+        # print("Helal be")
+
+    def the_button_was_clickedme(self):
+        self.button.setText("You already clicked me.")
+        self.button.setEnabled(False)
+    def togled(self,checked):
+        print("Checked?",checked)
+
+        self.setWindowTitle("My Oneshot App")
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
+"""
+
+
+
+
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QVBoxLayout, QWidget
+
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        self.label = QLabel()
+
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(container)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
